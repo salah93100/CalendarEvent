ea1b4dbeefe62cf45f0891b52424cb795c45bf95
salah93100 <salah.elbouhali@gmail.com>
2024-07-13 20:03:24 +0200

    build event compoment

--- calendar-app/src/App.js
+++ calendar-app/src/App.js
@@ -1,8 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import Event from "./Event.js";
+import input from "./input.json";
 function App() {
-  return <div className="App">-9H -21h</div>;
+  const Height = window.innerHeight;
+
+  return (
+    <div className="App">
+      <div
+        style={{
+          top: `0px`,
+          position: "absolute",
+        }}
+      >
+        -9H
+      </div>
+
+      {input.map((inputEvent) => (
+        <Event
+          eventID={inputEvent.id}
+          start={inputEvent.start}
+          duration={inputEvent.duration}
+          screenHeight={Height}
+        />
+      ))}
+      <div
+        style={{
+          top: "800px",
+          position: "absolute",
+        }}
+      >
+        -21H
+      </div>
+    </div>
+  );
 }
 
 export default App;
